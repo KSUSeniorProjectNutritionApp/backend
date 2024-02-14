88e3560238ee9a3ebc6319866e74ea7602210fd0
Michael Ehme <mc.ehme@gmail.com>
2024-02-14 16:26:38 -0500

    Removing unnecessary repos

--- src/main/java/edu/kennesaw/repositories/FoodNutrientRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.kennesaw.repositories;
-
-import edu.kennesaw.POJO.FoodNutrient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodNutrientRepository extends JpaRepository<FoodNutrient, Integer> {
-}
